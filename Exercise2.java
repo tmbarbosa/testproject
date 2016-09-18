80934353be74591ff6fe3d71d0158675776ee1b6
Thiago2 Merino <thiago.merino@uol.com.br>
2016-09-18 11:17:29 -0300

    Added new files: Exercise2.java, .gitignore. Updates README.md.

---
+++ Exercise2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thiagomerino
+ */
+public class Exercise2 {
+    public static void main(String[] args){
+        int[] vetor = new int[10];
+        int soma = 0;
+        
+        for(int i = 0; i < 10; i++)
+            vetor[i] = i;
+        
+        for(int i = 0; i < 10; i++)
+           soma = soma + vetor[i];
+        
+        float media = (float)soma/vetor.length;
+        System.out.println("Media dos valores: " +media);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # testproject
 Test project to learn git command use
+Added Exercise2.java
